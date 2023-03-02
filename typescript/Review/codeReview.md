d4d1b34a18279b7676eeb7a275a4f1c18eae6f9a
Puyol Antoine <apuyol@hotmail.fr>
2023-03-02 10:33:26 +0100

    correction review

--- typescript/Review/codeReview.md
+++ typescript/Review/codeReview.md
@@ -8,8 +8,8 @@ On peut également effectuer des calculs sur ces monnaies comme des additions, d
 
 # Deuxième code review => qu'est ce qui ne va pas dans le code #
 
-La première chose qui ne va pas est le paramètre non utilisé dans les méthodes Add, Devide et Times.
+La première chose qui ne va pas est le paramètre non utilisé dans les méthodes Add, Divide et Times.
 
-Il faudrait modifier cela, pour que l'on puisse faire ces calculs sur des monnaies différents. Car pour le moment le code par du principe que ces calculs ne se font que sur des monnaie similaire.
+Il faudrait modifier cela, pour que l'on puisse faire ces calculs sur des monnaies différentes. Car pour le moment le code par du principe que ces calculs ne se font que sur des monnaies similaires.
 
-La seconde -> quand on rajoute une exchangeRate cela crée à chaque fois une nouvelle banque et donc on ne peut pas stocker plusieurs exchange, et on ne peut pas utiliser plusieurs fois le même sans créer une nouvelle banque à chaque fois.
+La seconde -> quand on rajoute un exchangeRate cela crée à chaque fois une nouvelle banque et donc on ne peut pas stocker plusieurs exchange, et on ne peut pas utiliser plusieurs fois le même sans créer une nouvelle banque à chaque fois.
--- typescript/src/Bank.ts
+++ typescript/src/Bank.ts
@@ -1,5 +1,5 @@
-import {Currency} from './Currency'
-import {MissingExchangeRateError} from './MissingExchangeRateError'
+import { Currency } from './Currency'
+import { MissingExchangeRateError } from './MissingExchangeRateError'
 
 export class Bank {
   private readonly _exchangeRates: Map<string, number> = new Map()
@@ -9,7 +9,7 @@ export class Bank {
    * @param currency2
    * @param rate
    */
-  static withExchangeRate (currency1: Currency, currency2: Currency, rate: number): Bank {
+  static withExchangeRate(currency1: Currency, currency2: Currency, rate: number): Bank {
     const bank = new Bank()
     bank.AddExchangeRate(currency1, currency2, rate)
     return bank
@@ -20,7 +20,7 @@ export class Bank {
    * @param currency2
    * @param rate
    */
-  AddExchangeRate (currency1: Currency, currency2: Currency, rate: number): void {
+  AddExchangeRate(currency1: Currency, currency2: Currency, rate: number): void {
     this._exchangeRates.set(currency1 + '->' + currency2, rate)
   }
 
@@ -29,11 +29,11 @@ export class Bank {
    * @param currency1
    * @param currency2
    */
-  Convert (amount: number, currency1: Currency, currency2: Currency): number {
+  Convert(amount: number, currency1: Currency, currency2: Currency): number {
     if (!(currency1 === currency2 || this._exchangeRates.has(currency1 + '->' + currency2))) { throw new MissingExchangeRateError(currency1, currency2) }
 
     return currency2 === currency1
-        ? amount
-        : amount * this._exchangeRates.get(currency1 + '->' + currency2)
+      ? amount
+      : amount * this._exchangeRates.get(currency1 + '->' + currency2)
   }
 }
