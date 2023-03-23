c2a3f899e1bac02c3f7150fd0245b8d6715f1900
Puyol Antoine <apuyol@hotmail.fr>
2023-03-23 14:27:53 +0100

    test money add

--- typescript/tests/Money.spec.ts
+++ typescript/tests/Money.spec.ts
@@ -3,7 +3,7 @@ import { Money } from "../src/Money";
 
 describe('Money', function () {
 
-    test('multiply EUR by 2 and return twice the amount in EUR', () => {
+    test('multiply 2 EUR by 2 and return twice the amount in EUR', () => {
       //Arrange
       let money = new Money(2, Currency.EUR);
   
@@ -14,6 +14,18 @@ describe('Money', function () {
       expect(money.currency).toBe(Currency.EUR);
       expect(money.value).toBe(4)
     })
+
+    test('Add 5 USD to 90 USD and return 95 USD', () => {
+        //Arrange
+        let money = new Money(90, Currency.USD);
+        
+        //Act
+        money.add(new Money(5, Currency.USD));
+    
+        //Assert
+        expect(money.currency).toBe(Currency.USD);
+        expect(money.value).toBe(95)
+      })
   
    
   })
