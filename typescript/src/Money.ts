dfc68c9e457c11e59d8f375921cce81cb3bfe93b
Puyol Antoine <apuyol@hotmail.fr>
2023-03-23 14:29:35 +0100

    test money add passed

--- typescript/src/Money.ts
+++ typescript/src/Money.ts
@@ -6,22 +6,24 @@ export class Money {
     public value: number;
     public currency: Currency;
 
-    constructor(value: number, currency: Currency){
+    constructor(value: number, currency: Currency) {
         this.value = value;
         this.currency = currency;
     }
 
     public add(amount: Money) {
-        if(amount.currency != this.currency) {
+        if (amount.currency != this.currency) {
             throw new WrongTypeOfCurrencyError(amount.currency, this.currency);
+        } else {
+            this.value += amount.value;
         }
     }
 
-    public times(amount: number){
-        if(amount < 0){
+    public times(amount: number) {
+        if (amount < 0) {
             throw new NegativeValueError();
-        }else{
-            this.value = amount*this.value; 
+        } else {
+            this.value = amount * this.value;
         }
     }
 }
